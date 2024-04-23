cdcd750bee7d509c5eca6eefffc83fe488db5059
jgchoti <jgchotirat@gmail.com>
2024-04-23 14:06:59 +0200

    run format

--- src/intersection/intersection.spec.js
+++ src/intersection/intersection.spec.js
@@ -130,17 +130,11 @@ const cases = [
 // run solution to test
 // add name of functions to run test
 for (const solution of [filterIntersection]) {
-  describe(
-    `${solution.name
-    }: return an array of values that are in both the first and the second arrays`,
-    () => {
-      for (const caseInfo of cases) {
-        it(caseInfo.label, () => {
-          expect(solution(caseInfo.argA, caseInfo.argB)).toEqual(
-            caseInfo.answer,
-          );
-        });
-      }
-    },
-  );
+  describe(`${solution.name}: return an array of values that are in both the first and the second arrays`, () => {
+    for (const caseInfo of cases) {
+      it(caseInfo.label, () => {
+        expect(solution(caseInfo.argA, caseInfo.argB)).toEqual(caseInfo.answer);
+      });
+    }
+  });
 }
